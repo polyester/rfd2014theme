3de3d6046d96e0c8f91af21584f2d84642303381
Paul Roeland <paul@cleanclothes.org>
2014-01-12 18:29:48 +0100

    getting it slowly ready

--- quintagroup/theme/pythonreel/static/config.rb
+++ quintagroup/theme/pythonreel/static/config.rb
@@ -7,7 +7,8 @@ sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-#output_style = :compressed
+output_style = :compressed
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
